1c9fd43204d01ae2e686aa11ae03c32542a42e1d
jxxll <111830505+jxxll@users.noreply.github.com>
2025-03-07 21:18:18 +0800

    Update create-orthology-list.sh

--- create-orthology-list.sh
+++ create-orthology-list.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-# 检查必要的依赖是否安装
-command -v orthofinder >/dev/null 2>&1 || { echo >&2 "OrthoFinder 未安装. 请先安装 OrthoFinder."; exit 1; }
-command -v mafft >/dev/null 2>&1 || { echo >&2 "MAFFT 未安装. 请先安装 MAFFT."; exit 1; }
-command -v FastTree >/dev/null 2>&1 || { echo >&2 "FastTree 未安装. 请先安装 FastTree."; exit 1; }
-command -v phylopypruner >/dev/null 2>&1 || { echo >&2 "phylopypruner 未安装. 请先安装 phylopypruner."; exit 1; }
-command -v reciprologs >/dev/null 2>&1 || { echo >&2 "reciprologs 未安装. 请先安装 reciprologs."; exit 1; }
+# Help information
+usage() {
+    echo "Usage: $0 -d <input_directory> -t <threads> -q <reciprocal_threshold> -o <output_file>"
+    exit 1
+}
 
+# Parse command line parameters
+while getopts "d:t:q:o:a:b:" opt; do
+    case $opt in
+        d) input_dir="$OPTARG" ;;
+        t) threads="$OPTARG" ;;
+        q) reciprocal_threshold="$OPTARG" ;;
+        o) output_file="$OPTARG" ;;       
+        *) usage ;;
+    esac
+done
 
-echo "step1: 使用 OrthoFinder 构建跨物种的直系同源组"
-orthofinder -f files/ -t 20 -S mmseqs -M msa
+# Check if necessary parameters are provided
+if [ -z "$input_dir" ] || [ -z "$threads" ] || [ -z "$reciprocal_threshold" ] || [ -z "$output_file" ] ; then
+    usage
+fi
 
+# Step 1: Build cross-species orthogroups using OrthoFinder
+echo "Step 1: Build cross-species orthogroups using OrthoFinder"
+ls $input_dir/* | awk -F'/' '{print $NF}' > species
+for i in $input_dir/*; do
+    species=$(basename "$i")  
+    sed -i 's/>/>\"$species\"/g' $i;
+done
 
-echo "step2: 使用 Orthogroup_Sequences 进行比对并创建系统发育树"
+orthofinder -f "$input_dir" -t "$threads" -S mmseqs -M msa
+
+# Step 2: Align and create phylogenetic trees using Orthogroup_Sequences
+echo "Step 2: Align and create phylogenetic trees using Orthogroup_Sequences"
+
+cd $input_dir/OrthoFinder/Results_*/Orthogroup_Sequences
 for i in *.fa; do
     mafft --maxiterate 1000 --localpair "$i" > "${i%%.fa}.fas"
 done
+
 for i in *.fas; do
     FastTree "$i" > "${i%%.fas}.nw"
 done
 
-
-echo "step3: 使用 phylopypruner 修剪系统发育树以获得单细胞系统发育树"
+# Step 3: Prune phylogenetic trees to get single-cell phylogenetic trees using phylopypruner
+echo "Step 3: Prune phylogenetic trees to get single-cell phylogenetic trees using phylopypruner"
 for i in *.fas; do
     base_name="${i%%.fas}"
     mkdir -p "$base_name"
@@ -30,15 +54,36 @@ for i in *.fas; do
         mv "${base_name}.nw" "$base_name/"
     fi
 done
-
+rm *.fa
 for i in */; do
-    phylopypruner --dir "$i" --min-len 100 --trim-lb 4 --min-support 0.75 --prune MI --min-taxa 2 --min-otu-occupancy 0.0 --min-gene-occupancy 0.0
+    phylopypruner --dir "$i" --min-len 100 --trim-lb 4 --min-support 0.75 --prune MI --min-taxa 2 --min-otu-occupancy 0.8 --min-gene-occupancy 0.0
 done
 
+echo "Step 4: create homologous gene list"
 
-echo "step4: 将所有 /phylopypruner_output/output_alignments 结果汇总成一个表格以显示直系同源"
+cp */phylopypruner_output/output_alignments/*fas ../fas
+cd ../fas
+mapfile -t species_list < species
 
+output_file="$output_dir"/orthology-genelist.csv
+echo "Orthogroup,$(IFS=,; echo "${species_list[*]}")" > "$output_file"
 
+# 遍历所有.fas文件
+for file in *.fas; do
+    filename=$(basename "$file")
+    row_content="$filename"
 
+    for species in "${species_list[@]}"; do
+        escaped_species=$(sed 's/[][\.^$*]/\\&/g' <<< "$species")
+        
+        gene_id=$(sed -n "/^>${escaped_species}@/{s/^>${escaped_species}@//p; q}" "$file")
+        
+        [[ -z "$gene_id" ]] && gene_id=""
+        row_content="${row_content},${gene_id}"
+    done
+    
+    # 追加结果到CSV文件
+    echo "$row_content" >> "$output_file"
+done
 
-echo "All done."
+echo "All steps are completed."
